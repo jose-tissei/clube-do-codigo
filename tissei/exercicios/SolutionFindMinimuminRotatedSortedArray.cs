a121d4ed65c0be955fb78674c4e1211f165d9537
José Tissei <z.94@live.com>
2025-08-17 12:02:53 -0300

    otimizando solucao

--- tissei/exercicios/SolutionFindMinimuminRotatedSortedArray.cs
+++ tissei/exercicios/SolutionFindMinimuminRotatedSortedArray.cs
@@ -6,6 +6,43 @@ using System.Threading.Tasks;
 
 namespace exercicios
 {
+    public class SolutionDoBixao
+    {
+        public int FindMin(int[] nums)
+        {
+
+            var left = 0;
+            var right = nums.Length - 1;
+            var result = nums[0];
+
+            while (left <= right)
+            {
+                if (nums[left] < nums[right])
+                {
+                    return Math.Min(nums[left], result);
+                }
+
+                var mid = (left + right) / 2;
+                result = Math.Min(nums[mid], result);
+
+                if (nums[mid] >= nums[left])
+                {
+                    result = Math.Min(nums[left], result);
+                    left = mid + 1;
+                }
+                else
+                {
+                    result = Math.Min(nums[right], result);
+                    right = mid - 1;
+                }
+
+            }
+
+            return result;
+        }
+    }
+
+
     public class SolutionFindMinimuminRotatedSortedArray
     {
         public int FindMin(int[] nums)
@@ -13,19 +50,20 @@ namespace exercicios
 
             var left = 0;
             var right = nums.Length - 1;
-            var mid = (int)Math.Floor(right / 2M);
+            var mid = right / 2;
 
             while (left <= right && left != mid)
             {
                 if (nums[right] < nums[left] && nums[right] < nums[mid])
                 {
                     left = mid;
-                    mid = (int)Math.Floor((mid + right) / 2M);
+                    mid = (mid + right) / 2;
+                    // Division between two ints uses integer division, which truncates (rounds toward zero)
                 }
                 else
                 {
                     right = mid;
-                    mid = (int)Math.Floor((mid + left) / 2M);
+                    mid = (mid + left) / 2;
                 }
             }
 
